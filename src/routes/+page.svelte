4b6f3dac4adf9026db345805fabfb3457c6f1e8a
Basic-beep <elliottroseemail@gmail.com>
2025-07-26 11:18:36 -0400

    trying it from the beginning with clerk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+<script lang="ts">
+	import { SignedIn, SignedOut, SignInButton, UserButton } from 'svelte-clerk';
+</script>
+
 <style>
     h1 {text-align: center;}
     h2 {text-align: center;}
@@ -10,7 +14,19 @@
         text-align: center;
     }
 </style>
+<template>
+	<header>
+		<SignedOut>
+			<SignInButton />
+		</SignedOut>
+		<SignedIn>
+			<UserButton />
+		</SignedIn>
+	</header>
+</template>
+
 <html lang="en">
+
 
 <head>
     <meta charset="UTF-8">
@@ -20,6 +36,7 @@
 <body style="background-color:#90ee90">
     <h1 style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">Welcome To HikeBuddy!</h1>
     <h2 style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">Track your hikes and milage</h2>
+    <SignInButton></SignInButton>
     <div class="footer">
         <p>
             Made with ❤️ by BasicBirdy
